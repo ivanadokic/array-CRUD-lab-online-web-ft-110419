4ef8d10b6c31e00d2a911597cafe5981ac80e7fe
Ivana <ivana_pile@yahoo.com>
2019-11-05 23:27:26 -0500

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
-def create_an_empty_array
-  
-end
 
-def create_an_array
+    def create_an_empty_array
+      []
+    end
+    
+    def create_an_array
+        ["dog", "cat", "fish", "owl"]
+    end
   
-end
+    def add_element_to_end_of_array(array, element)
 
-def add_element_to_end_of_array(array, element)
-  
-end
+        array = ["dog", "cat", "fish", "owl"]
+        array << ("arrays!")
+    end	
 
-def add_element_to_start_of_array(array, element)
   
-end
+  def add_element_to_start_of_array(array, element)
+    array.unshift ("wow")
+  end
+  
+  def remove_element_from_end_of_array(array)
+  array.pop
 
-def remove_element_from_end_of_array(array)
+  end
   
-end
-
-def remove_element_from_start_of_array(array)
+  def remove_element_from_start_of_array(array)
+     dog = array.shift 
+  end
   
-end
-
-def retrieve_element_from_index(array, index_number)
+  def retrieve_element_from_index(array, index_number)
+    array[index_number]
+  end
   
-end
-
-def retrieve_first_element_from_array(array)
+  def retrieve_first_element_from_array(array)
+  array.first
   
-end
-
-def retrieve_last_element_from_array(array)
+  def retrieve_last_element_from_array(array)
+array.last
+  end
   
-end
